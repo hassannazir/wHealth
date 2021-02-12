3b13cba96a5dbdebc6d706c0d6c5b9658e62db5f
awais519 <bsef17m519@pucit.edu.pk>
2021-02-12 17:42:00 +0500

    EMAIL VERIFICATION VIEW SET!!!

--- wHealthApi/Controllers/EmailVerificationController.cs
+++ wHealthApi/Controllers/EmailVerificationController.cs
@@ -6,37 +6,62 @@ using System.Linq;
 using System.Threading.Tasks;
 using wHealthApi.Models;
 
+
 namespace wHealthApi.Controllers
 {
-    public class EmailVerificationController :BaseController
-    {      
+    public class EmailVerificationController : Controller
+    {
 
         private readonly wHealthappDbContext _context;
         public EmailVerificationController(wHealthappDbContext context)
         {
             _context = context;
         }
-      
 
-       
-        [HttpGet]
+
+
+        //[HttpGet]
+        //[AllowAnonymous]
+        //public async Task<IActionResult> VerifyAsync(int id)
+        //{
+        //    var us = await _context.Users.FindAsync(id);
+
+        //    if (us != null)
+        //    {
+        //        us.Status = "Active";
+        //        await _context.SaveChangesAsync();
+        //    }
+
+        //    return Ok();
+        //}
+
+
+
+        [HttpGet("id")]
         [AllowAnonymous]
-        public async Task<IActionResult> VerifyAsync(int id)
+        public IActionResult Verify(String id)
         {
-            var us = await _context.Users.FindAsync(id);
+
+
+            var us = _context.Users.Where(u => u.Username == id).FirstOrDefault();
+
+
 
             if (us != null)
             {
+
                 us.Status = "Active";
-                await _context.SaveChangesAsync();
+                _context.SaveChanges();
+                return Ok("verification success!!!! and now you can login the app......");
+
             }
 
-            return Ok();
+            return Ok("VERFICATION FAILED!!!");
+
+
+
+
         }
-
-
-
-        
 
 
     }
