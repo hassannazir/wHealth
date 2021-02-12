ccc44f52965f9abaa9fd40ab94db75b6fbf7bea0
awais519 <bsef17m519@pucit.edu.pk>
2021-02-12 23:39:14 +0500

    EMAIL VERIFICATION FINAL VERSION!!!! KOI BANDA AB PANGA NA LA IS KA SATH....

--- wHealthApi/Controllers/UsersController.cs
+++ wHealthApi/Controllers/UsersController.cs
@@ -196,7 +196,9 @@ namespace wHealthApi.Controllers
                     em.To.Add(appUser.Email);
                     em.From = new MailAddress("dev@theta.solutions","wHealth");
                     em.Subject = "EMAIL VERFICATION FOR wHEALTH APP.";
-                    em.Body = "<h4>Dear " + appUser.Name +", </h4><br>Please click on the following link to confirm your email.<br><br>" + "<a href='https://whealthapp.azurewebsites.net/api/EmailVerification?id=" + user.Id + "'>CLICK HERE! </a><br><br>  Thanks.";
+
+                   // em.Body = "<h4>Dear " + appUser.Name + ", </h4><br>Please click on the following link to confirm your email.<br><br>" + "<a href='https://localhost:44340/Id?Id=" + user.Username + "'>CLICK HERE! </a><br><br>  Thanks.";
+                    em.Body = "<h4>Dear " + appUser.Name + ", </h4><br>Please click on the following link to confirm your email.<br><br>" + "<a href='http://whealthapi.azurewebsites.net/Id?Id=" + user.Username + "'>CLICK HERE! </a><br><br>  Thanks.";
 
                     em.IsBodyHtml = true;
 
