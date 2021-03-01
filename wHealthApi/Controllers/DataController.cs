0685400338e1d372f45b7fe91701a8beaa3f6b9c
awais519 <bsef17m519@pucit.edu.pk>
2021-03-01 18:39:10 +0500

    GET CLINICS DATA API --- CORRECTED

--- wHealthApi/Controllers/DataController.cs
+++ wHealthApi/Controllers/DataController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using wHealthApi.Models;
-
+using wHealthApi.Models.JoinModels;
 
 namespace wHealthApi.Controllers
 {
@@ -25,9 +25,19 @@ namespace wHealthApi.Controllers
         {
             try
             {
-
+                Response res = new Response();
                 IList<Clinic> ClinicList = _context.Clinics.ToList();
-                return Ok(ClinicList);
+                var query = (from c in _context.Clinics
+                            join u in _context.Users
+                            on c.Id equals u.ClinicId
+                            where u.Status == "Active" 
+                            select new { c.Email, c.Name, c.PhoneNo, c.RegistrationNo, c.Id, c.Address }).ToList();
+
+                res.Status = true;
+                res.Result = query;
+                res.Message = "FOLLOWING CLINICS AVAILABLE";
+                return Ok(res);
+
             }
             catch (Exception ex)
             {
@@ -42,9 +52,13 @@ namespace wHealthApi.Controllers
         {
             try
             {
-                var ClinicData = _context.Clinics.Find(id);
+                Response res = new Response();
 
-                return Ok(ClinicData);
+                var ClinicData = _context.Clinics.Find(id);
+                res.Message = "Selected Clinic's data.";
+                res.Result = ClinicData;
+                res.Status = true;
+                return Ok(res);
             }
             catch (Exception ex)
             {
