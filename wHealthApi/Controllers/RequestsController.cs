6ba6d32374a723a09bf466549efbe42b02919d50
awais519 <bsef17m519@pucit.edu.pk>
2021-03-01 22:29:58 +0500

    THE DOCTORS STATUS GET APPROVED BY THE CLINICS AND AN EMAIL WILL BE SENT TO THAT DOCTOR TELLING THAT HIS/HER REQUEST HAS BEEN APPROVED TO WORK IN THE CLINIC.

--- wHealthApi/Controllers/RequestsController.cs
+++ wHealthApi/Controllers/RequestsController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using wHealthApi.Models;
 using wHealthApi.Models.JoinModels;
@@ -19,7 +21,49 @@ namespace wHealthApi.Controllers
         }
 
 
-        
+
+        [HttpPut]
+        [AllowAnonymous]
+        public async Task<IActionResult> DoctorClinicApprovalAsync(int cid, int did)
+        {
+
+            try
+            {
+                var DocWhoWillWork = _context.Doctorclinics.Where(e => e.ClinicId == cid && e.DoctorId == did).FirstOrDefault();
+                var doc = _context.Doctors.Where(e => e.Id == did).FirstOrDefault();
+                var clin = _context.Clinics.Where(e => e.Id == cid).FirstOrDefault();
+
+                DocWhoWillWork.Status = "Active";  //DOCTOR WILL GET TO WORK IN THE SPECIFIED CLINIC
+                _context.SaveChanges();
+                MailMessage em = new MailMessage();
+                em.To.Add(doc.Email);
+                em.From = new MailAddress("dev@theta.solutions", "wHealth");
+                em.Subject = "REQUEST TO WORK IN CLINIC";
+
+                // em.Body = "<h4>Dear " + appUser.Name + ", </h4><br>Please click on the following link to confirm your email.<br><br>" + "<a href='https://localhost:44340/Id?Id=" + user.Username + "'>CLICK HERE! </a><br><br>  Thanks.";
+                em.Body = "<h4>Dear " + doc.Name + ", </h4><br>YOUR REQUEST TO WORK FOR THE CLINIC "+clin.Name+" ,  YOU SELECTED HAS BEEN APPROVED. NOW YOU CAN WORK IN THE CLINIC. CONGRATULATIONS!!!!<br><br>";
+
+                em.IsBodyHtml = true;
+
+                SmtpClient cli = new SmtpClient();
+                cli.Host = "116.202.175.92";
+                cli.Port = 25;
+                cli.UseDefaultCredentials = false;
+                cli.Credentials = new NetworkCredential("dev@theta.solutions", "8xTc$NU?%5kCh.5L&,u#:o^S|oCp");
+                cli.EnableSsl = false;
+                await cli.SendMailAsync(em);
+                Response r = new Response();
+                r.Message = "You approved the request, Successfully.";
+                return Ok(r);
+
+
+            }
+            catch
+            {
+                throw;
+            }
+            
+        }
 
     }
 }
